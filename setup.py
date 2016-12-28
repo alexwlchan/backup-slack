cff9cd1e7747ed2f2981f5ef052e8d88e38995eb
Alex Chan <alex@alexwlchan.net>
2016-12-28 10:22:04 +0000

    1.0 = production ready

--- setup.py
+++ setup.py
@@ -41,7 +41,7 @@ setup(
 
     # See https://pypi.python.org/pypi?%3Aaction=list_classifiers
     classifiers=[
-        'Development Status :: 4 - Beta',
+        'Development Status :: 5 - Production/Stable',
         'Intended Audience :: Other Audience',
         'License :: OSI Approved :: MIT License',
         'Programming Language :: Python :: 2',
